a5d962803b85a55dca51db6af9563c86b3327bee
TLtlbo <js1436kt@gmail.com>
2023-08-06 17:37:07 +0900

    Initial Commit

---
+++ WarningMessageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarningMessageApp
+//
+//  Created by 박진성 on 2023/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
